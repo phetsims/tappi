303eb3a3af5c2f95d2cc36356cb970948fe9c73d
samreid <reids@colorado.edu>
2020-02-27 12:08:01 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/tappi-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import tappi from './tappi.js';
+
+const tappiStrings = getStringModule( 'TAPPI' );
+
+tappi.register( 'tappiStrings', tappiStrings );
+
+export default tappiStrings;
