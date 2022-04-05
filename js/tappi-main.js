b89410ccb0b372bf43d9065501d0383bb0897fce
Jesse <jesse.greenberg@colorado.edu>
2022-04-05 09:53:15 -0400

    add note about removing vibrationManager from Sim.js options, see https://github.com/phetsims/joist/issues/794

--- js/tappi-main.js
+++ js/tappi-main.js
@@ -12,7 +12,7 @@ import Tandem from '../../tandem/js/Tandem.js';
 import BasicsScreen from './demo/basics/BasicsScreen.js';
 import PatternsScreen from './demo/patterns/PatternsScreen.js';
 import tappiStrings from './tappiStrings.js';
-import vibrationManager from './vibrationManager.js';
+// import vibrationManager from './vibrationManager.js';
 
 const tappiTitleString = tappiStrings.tappi.title;
 
@@ -26,9 +26,14 @@ const simOptions = {
     graphicArts: '',
     soundDesign: '',
     thanks: ''
-  },
+  }
 
-  vibrationManager: vibrationManager
+  // NOTE: Sim.js no longer takes this option, but this was the way we passed it to the sim to test HTML5 vibration.
+  // It was an option for Sim.js so that we didn't create dependencies on tappi because it is experimental. But
+  // TypeScript requires references for typing so we decided to just remove the option for now since tappi is
+  // abandoned for the most part. If coming back to this, look into initializing and stepping the vibrationManager.
+  // See https://github.com/phetsims/joist/issues/794
+  // vibrationManager: vibrationManager
 };
 
 // launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
