cc4d2ba395fce73eff635de65b4c11bda7e883fe
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:43 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/tappiStrings.ts
+++ js/tappiStrings.ts
@@ -5,11 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import tappi from './tappi.js';
 
 type StringsType = {
   'tappi': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   }
 };
 
