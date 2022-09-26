9c1a934c441638ca0f5c8c38ea397a9fc9d1c141
Sam Reid <reids@colorado.edu>
2022-09-26 12:17:08 -0600

    Change TReadOnlyProperty => LinkableProperty, see https://github.com/phetsims/axon/issues/414

--- js/TappiStrings.ts
+++ js/TappiStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
+import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import tappi from './tappi.js';
 
 type StringsType = {
   'tappi': {
     'title': string;
-    'titleStringProperty': TReadOnlyProperty<string>;
+    'titleStringProperty': LinkableProperty<string>;
   }
 };
 
