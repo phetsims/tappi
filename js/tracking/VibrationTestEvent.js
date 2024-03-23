475688118ffb00797a553f290876e08a285e3387
phet-dev <phetadmin@gmail.com>
2024-03-23 03:29:07 -0600

    update copyright dates from daily grunt work

--- js/tracking/VibrationTestEvent.js
+++ js/tracking/VibrationTestEvent.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2024, University of Colorado Boulder
 
 /**
  * A collection of data related to user input, for the purposes of the Vibration project. While
