b932fa4c6022c5a4311461f979446ba34a5162ac
phet-dev <phetadmin@gmail.com>
2024-09-24 04:00:01 -0600

    update copyright dates from daily grunt work

--- js/VibrationPatterns.ts
+++ js/VibrationPatterns.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * Collection of reusable patterns that may be useful with vibrationManager. For example, try
--- js/vibrationManager.ts
+++ js/vibrationManager.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * A singleton that manages vibration feedback through the web vibration API. navigator.vibrate is required
