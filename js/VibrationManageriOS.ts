1bdcae37481e44b4f1846307d59188f6372de170
phet-dev <phetadmin@gmail.com>
2025-02-14 03:45:44 -0700

    update copyright dates from daily grunt work

--- js/VibrationManageriOS.ts
+++ js/VibrationManageriOS.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2020-2025, University of Colorado Boulder
 
 /**
  * A class that controls vibrations when running the sim in a native iOS App. It sends messages form the simulation
